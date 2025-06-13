f72519b2cb869db3c34e6ea3dc14076648a1f271
Gangia <jwlee03171@naver.com>
2025-06-13 11:25:39 +0900

    Remove test file

--- chal_v3/automotive_ctf/engine_ecu/uds_service.py
+++
@@ -1,7 +0,0 @@
-class UDSService:
-    def __init__(self, ecu):
-        self.ecu = ecu
-        
-    def process_request(self, data):
-        # Basic UDS implementation
-        return bytes([0x7F, data[0], 0x11])
